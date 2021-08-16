1989692ee364902b23a35f3c943951fe3cd63278
ansarali41 <ansar.ict14@gmail.com>
2021-08-17 01:25:35 +0600

    published

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from "./Components/Login";
+import Login from './Components/Login';
 import './assets/css/login.css';
 
 function App() {
-  return (
-    <>
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-         
-        </Switch>
-      </div>
-
-    </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <div className="App">
+                    <Switch>
+                        <Route exact path="/">
+                            <Login />
+                        </Route>
+                        <Route path="/home">
+                            <Login />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </>
+    );
 }
 
 export default App;
-
